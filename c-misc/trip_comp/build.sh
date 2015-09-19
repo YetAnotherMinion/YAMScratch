32499d45842662b1fa4cdd04f94c394df32082d3
YetAnotherMinion <yetanotherminion@gmail.com>
2015-09-19 10:15:51 -0400

    passed first stage

---
+++ c-misc/trip_comp/build.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+g++  -g -Wall -Wextra -pthread --pedantic-errors --std=c++11  stage_0.cc -o stage_0
---
+++ c-misc/trip_comp/stage_0.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+
+using namespace std;
+
+#define REP(i,n) for((i)=0;(i)<(int)(n);(i)++)
+#define foreach(c,itr) for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
+
+typedef long long ll;
+#define MOD 1000003ll
+
+
+int main(int argc, char const *argv[])
+{
+	printf("C Hello World\n");
+	cout << "C++ Hello World" << endl;
+	return 0;
+}
