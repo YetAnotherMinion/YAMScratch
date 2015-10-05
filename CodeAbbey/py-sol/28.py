93867ebce4a97e948b2c908c7170c2d3a792ce88
YetAnotherMinion <yetanotherminion@gmail.com>
2015-10-05 08:50:25 -0400

    CA 28 in python verified

---
+++ CodeAbbey/py-sol/28.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(raw_input())
+for _ in xrange(0, N):
+	line = raw_input().split()
+	assert 2 == len(line)
+	(weight, height) = line
+	bmi = float(weight) / (float(height) ** 2) 
+	if bmi < 18.5:
+		description = 'under'
+	elif bmi < 25:
+		description = 'normal'
+	elif bmi < 30:
+		description = 'over'
+	else:
+		description = 'obese'
+	sys.stdout.write(description + ' ')
+sys.stdout.write('\n')
