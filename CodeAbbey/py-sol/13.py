69e728ebf26fd3bc2fcbd55392f2321e6ffc63b7
YetAnotherMinion <yetanotherminion@gmail.com>
2015-10-19 23:49:51 -0400

    CA 13 in python verified

--- CodeAbbey/py-sol/13.py
+++ CodeAbbey/py-sol/13.py
@@ -1,10 +1,8 @@
 #first line of input is number of test cases
 N = int(raw_input())
 assert N > 0
+#second line contains N test cases separated by whitespace
 line = [int(x) for x in raw_input().split()]
 assert len(line) == N
-k = 1776
-
-
-wsd = [ sum([(int(indx)+1) * int(digit) for indx,digit in enumerate(str(x)) ]) for x in line]
-print " ".join([str(x) for x in wsd])
+wsd = [ str(sum([(int(indx)+1) * int(digit) for indx,digit in enumerate(str(x)) ])) for x in line]
+print " ".join(wsd)
