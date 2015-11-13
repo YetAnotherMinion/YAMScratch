d7a2d97cfecdd89e955e3467c90ca812a0e382a1
YetAnotherMinion <yetanotherminion@gmail.com>
2015-11-13 09:43:17 -0500

    allow test script to operate from any directory

--- CodeAbbey/test_solutions.sh
+++ CodeAbbey/test_solutions.sh
@@ -3,39 +3,43 @@
 # on each solution per language
 # Arguments:
 #  $1  absolute path to directory containing test cases
+#  $2  absolute path to YAMScratch/CodeAbbey
 
 echo $1
+echo $2
+CA_BASEPATH=${2%/}
 
 #create a tmp dir to store output
 
 for full_filename in `find $1 -type f -name '*.in'`;
 do
-	BASEPATH=$(dirname "$full_filename")
+	TEST_BASEPATH=$(dirname "$full_filename")
 	filename=$(basename "$full_filename")
 	i="${filename%.*}"
-	echo $BASEPATH
+	echo $TEST_BASEPATH
+	echo $CA_BASEPATH
 	echo "================ $i ==================";
 	#test the C files
-	if [ -f "./c-sol/src/$i.c" ] ; then
-		make $i -C c-sol
-		./c-sol/bin/$i.out < $BASEPATH/$i.in > ./c-sol/tmp.$i.out
-		diff --brief $BASEPATH/$i.out ./c-sol/tmp.$i.out
+	if [ -f "$CA_BASEPATH/c-sol/src/$i.c" ] ; then
+		make $i -C $CA_BASEPATH/c-sol
+		$CA_BASEPATH/c-sol/bin/$i.out < $TEST_BASEPATH/$i.in > $CA_BASEPATH/c-sol/tmp.$i.out
+		diff --brief $TEST_BASEPATH/$i.out $CA_BASEPATH/c-sol/tmp.$i.out
 		if [ $? -eq 1 ] ; then
 			#show the diff
-			diff $BASEPATH/$i.out ./c-sol/tmp.$i.out
+			diff $TEST_BASEPATH/$i.out $CA_BASEPATH/c-sol/tmp.$i.out
 		else
 			echo ""
 			# echo "they are the same"
 		fi
 		#clean up the tmp file
-		rm ./c-sol/tmp.$i.out
+		rm $CA_BASEPATH/c-sol/tmp.$i.out
 	fi
-	if [ -f "./py-sol/$i.py" ] ; then
-		python ./py-sol/$i.py < $BASEPATH/$i.in> ./py-sol/tmp.$i.out
-		diff --brief $BASEPATH/$i.out ./py-sol/tmp.$i.out
+	if [ -f "$CA_BASEPATH/py-sol/$i.py" ] ; then
+		python $CA_BASEPATH/py-sol/$i.py < $TEST_BASEPATH/$i.in> $CA_BASEPATH/py-sol/tmp.$i.out
+		diff --brief $TEST_BASEPATH/$i.out $CA_BASEPATH/py-sol/tmp.$i.out
 		if [ $? -eq 1 ] ; then
 			#show the diff
-			diff $BASEPATH/$i.out ./py-sol/tmp.$i.out
+			diff $TEST_BASEPATH/$i.out $CA_BASEPATH/py-sol/tmp.$i.out
 		else
 			echo ""
 			# echo "they are the same"
