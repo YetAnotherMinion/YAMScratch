18981a11b194997365d2c20f97e035b84d42f600
YetAnotherMinion <yam@thinkalexandria.com>
2016-08-26 19:07:55 -0400

    added sanity checking of root node argument

--- python-misc/slicer/src/bin_packing/guillotine.rs
+++ python-misc/slicer/src/bin_packing/guillotine.rs
@@ -1,27 +1,74 @@
+#[derive(Debug)]
 pub struct GuillotineNode {
-    pub x0: usize,
-    pub y0: usize,
+    width: f64,
+    height: f64,
+    x0: f64,
+    y0: f64,
+    used: bool,
     up: Option<Box<GuillotineNode>>,
     right: Option<Box<GuillotineNode>>,
 }
 
 impl GuillotineNode {
-    pub fn new(x: usize, y: usize) -> GuillotineNode {
+    pub fn new(x0: f64, y0: f64, width: f64, height: f64) -> GuillotineNode {
         GuillotineNode {
-            x0: x,
-            y0: y,
+            width: width,
+            height: height,
+            x0: x0,
+            y0: y0,
+            used: false,
             up: None,
             right: None,
         }
     }
 
-    pub fn steve(x: usize, y: usize, u: Box<GuillotineNode>,
-                     r: Box<GuillotineNode>) -> GuillotineNode {
-        GuillotineNode {
-            x0: x,
-            y0: y,
-            up: Some(u),
-            right: Some(r),
+//    pub fn steve(x: usize, y: usize, u: Box<GuillotineNode>,
+//                     r: Box<GuillotineNode>) -> GuillotineNode {
+//        GuillotineNode {
+//            x0: x,
+//            y0: y,
+//            used: false,
+//            up: Some(u),
+//            right: Some(r),
+//        }
+//    }
+    
+    pub fn find_free_node(&self, width: f64, height: f64) ->
+        Option<&Box<GuillotineNode>> {
+            None        
+    }
+
+    pub fn split_node(root: &mut Box<GuillotineNode>,
+                      box_width: f64, box_height: f64)-> 
+            Option<&Box<GuillotineNode>> {
+        /* Depiction of splitting procedure below:
+		 *  |<-----root width------>|
+         * 	+-----------------------+ ^                                              
+         * 	|                       | |                                              
+         * 	|         up            | |                                               
+         * 	|                       | |                                               
+         * 	+--------+--------------+ root height 
+         * 	|  box/  |              | |                                               
+         * 	|new_root|     right    | |                                              
+         * 	+------- +--------------+ V
+		 * The root node is the current node, wihch creates two child nodes
+		 * when a box is inserted into the root. The root node then becomes the
+		 * same size as the box going in.
+         */
+		// sanity check the invariant that this node should be empty
+        match root.up {
+            Some(_) => return None,
+            None => {},
         }
+        match root.right {
+            Some(_) => return None,
+            None => {},
+        }
+        if(1==1) {
+
+        }
+
+        print!("{:?}\n", root.up);
+		Some(root)
     }
 }
--- python-misc/slicer/src/main.rs
+++ python-misc/slicer/src/main.rs
@@ -26,7 +26,8 @@ pub fn main() {
 //    let mut b = Node::new();
 //    let c = Node::new();
 //    b.left = Some(Box::new(c));
-    let a = Box::new(G::new(1, 2));
-    let b = Box::new(G::new(3, 4));
-    let c = G::steve(5, 6, a , b);
+    let mut a = Box::new(G::new(1.0, 2.0, 10.0, 10.0));
+    let b = Box::new(G::new(3.0, 4.0, 10.0, 10.0));
+    //let c = G::steve(5, 6, a , b);
+    G::split_node(&mut a, 4.0, 5.0);
 }
