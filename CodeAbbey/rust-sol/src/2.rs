e1eec26fa2f42199ba5b9ba7d942cee427978cfa
YetAnotherMinion <yam@thinkalexandria.com>
2016-08-27 10:16:42 -0400

    solved CA 2 in Rust

--- CodeAbbey/rust-sol/src/2.rs
+++ CodeAbbey/rust-sol/src/2.rs
@@ -1,5 +1,18 @@
 use std::io;
 
 fn main() {
-    
+   let stdin = io::stdin();
+   let mut buffer = String::new();
+   stdin.read_line(&mut buffer).unwrap();
+   let n: u64 = buffer.trim().parse::<u64>().ok().unwrap();
+
+   buffer.clear();
+   stdin.read_line(&mut buffer).unwrap();
+   let data: Vec<i64> = buffer.split_whitespace()
+                              .filter_map(|x| x.trim().parse::<i64>().ok())
+                              .collect();
+
+   assert_eq!(n as usize, data.len());
+   let sum: i64 = data.iter().fold(0, |acc, x| acc + x);
+   println!("{}", sum);
 }
