e73f4fd41fde8a31d433f90863e0bbd9131e676a
YetAnotherMinion <yam@thinkalexandria.com>
2016-08-28 14:13:47 -0400

    simple example of reading command line arguments with rust 1.11

---
+++ rust-misc/argument_example.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 3 {
+        println!("Useage: {} a b", args[0]);
+        return;
+    }
+    let a = args[1].parse::<i64>().unwrap();
+    let b = args[2].parse::<i64>().unwrap();
+    let result: i64 = a + b;
+    println!("{}", result);
+
+}
