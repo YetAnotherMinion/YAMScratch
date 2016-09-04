089802d59b4516a90029ec5d48507812c17c1e15
YetAnotherMinion <yam@thinkalexandria.com>
2016-09-04 13:04:12 -0400

    remove debugging print statements and rename INFINITY to avoid a compiler builtin value

--- cpp-misc/floyd_warshall/src/floyd_warshall.cpp
+++ cpp-misc/floyd_warshall/src/floyd_warshall.cpp
@@ -26,19 +26,17 @@ void print_edges(std::vector<Node<>>& nodes) {
 }
 
 void print_board(std::vector<std::vector<int64_t>> board) {
-    const auto INFINITY = std::numeric_limits<int64_t>::min();
+    const auto MY_INFINITY = std::numeric_limits<int64_t>::min();
     for(auto& row : board) {
         for(auto& val : row) {
-            if(val == INFINITY) {
+            if(val == MY_INFINITY) {
                 std::cout << std::setw(3) << "I";
             } else {
                 std::cout << std::setw(3) << val;
             }
-
         }
         std::cout << std::endl;
     }
-
 }
 
 int main() {
@@ -60,11 +58,10 @@ int main() {
         // edges are directed from x->y
         nodes[x-1].edges[y-1] = w;
     }
-    //print_edges(nodes);
     // HACK: Use int64_t min value to indicate infinite distance
-    const auto INFINITY = std::numeric_limits<int64_t>::min();
+    const auto MY_INFINITY = std::numeric_limits<int64_t>::min();
     std::vector<std::vector<int64_t>> dist(N, 
-                                           std::vector<int64_t>(N, INFINITY));
+                                           std::vector<int64_t>(N, MY_INFINITY));
     for(uint64_t i = 0; i < N; ++i) {
         dist[i][i] = 0;
     }
@@ -74,37 +71,25 @@ int main() {
             dist[node.self_index][edge.first] = edge.second;
         }
     }
-    print_board(dist);
-    // return 0;
+
     for(uint64_t k = 0; k < N; ++k) {
         for(uint64_t i = 0; i < N; ++i) {
             for(uint64_t j = 0; j < N; ++j) {
                 // check that each leg of the purposed shorter path exists
-                if ((dist[i][k] == INFINITY) || (dist[k][j] == INFINITY)) {
-                    std::cout << "skipping" << std::endl;
+                if ((dist[i][k] == MY_INFINITY) || (dist[k][j] == MY_INFINITY)) {
                     continue;
                 }
                 // not a new path
                 if(i == k || j == k) {
-                    std::cout << "not a new path" << std::endl;
                     continue;
                 }
                 auto candiate_path = dist[i][k] + dist[k][j];
-                std::cout << "candiate " << i << " -> " << j << " = "
-                    << candiate_path << " using " << i << " -> " << k << " -> "
-                    << j << std::endl;
-                if (INFINITY == dist[i][j] ||
+                if (MY_INFINITY == dist[i][j] ||
                         dist[i][j] > candiate_path) {
-                    std::cout << "Setting dist[" << i << "][" << j << "] = "
-                        << candiate_path << std::endl;
                     dist[i][j] = candiate_path;
                 }
                 // check for negative cycles
-                if ((dist[j][j] != INFINITY) && (dist[j][j] < 0)) {
-                    std::cout << "Negative cycle at step:" << k << '\n'
-                        << "\tIncludes node " << j+1 << '\n'
-                        << "\tValue: " << dist[j][j] << '\n'
-                        << "\tInfinity: " << INFINITY << std::endl;
+                if ((dist[j][j] != MY_INFINITY) && (dist[j][j] < 0)) {
                     exit(1);
                 }
             }
@@ -116,7 +101,7 @@ int main() {
         std::cin >> x >> y;
         // remember to translate from 1 based indexing of nodes to the zero
         // based format we use
-        int64_t distance = (dist[x-1][y-1] == INFINITY) ? -1 : dist[x-1][y-1];
+        int64_t distance = (dist[x-1][y-1] == MY_INFINITY) ? -1 : dist[x-1][y-1];
         std::cout << distance << std::endl;
     }
 
