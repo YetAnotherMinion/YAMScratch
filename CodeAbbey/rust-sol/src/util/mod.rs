7e8bc3a81498c7854062ccd8f97b6d09c6b11fef
YetAnotherMinion <yam@thinkalexandria.com>
2016-09-05 18:41:54 -0400

    add allow(dead_code) attribute to module code

--- CodeAbbey/rust-sol/src/util/mod.rs
+++ CodeAbbey/rust-sol/src/util/mod.rs
@@ -2,6 +2,7 @@ mod quicksort;
 
 pub use self::quicksort::min_three;
 
+#[allow(dead_code)]
 pub fn round(mut c: f64) -> i64 {
     c += if c < 0.0 { -0.5  } else { 0.5 };
     c as i64
--- CodeAbbey/rust-sol/src/util/quicksort.rs
+++ CodeAbbey/rust-sol/src/util/quicksort.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub fn min_three<T>(a: T, mut b: T, mut c: T) -> T where T: PartialOrd {
     if a < b {
         b = a;
